2ef333baf8b6fdde6fd619f732f415773ef339b5
Adrian Philipp <mail@adrian-philipp.com>
2015-11-22 21:27:31 +0100

    Adds nvm support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/functions/rprompt.fish
 . ~/.config/fish/functions/fish_user_key_bindings.fish
+. ~/.config/fish/functions/bass.fish
+. ~/.config/fish/functions/nvm.fish
 
 # Editor
 set -gx EDITOR vim
@@ -9,8 +11,12 @@ set -gx EDITOR vim
 # Custom scipts
 set -gx PATH ~/bin $PATH 
 
+# Language
 set -gx LC_ALL en_US.UTF-8 
 set -gx LANG en_US.UTF-8
+
+# NVM
+bass source /usr/local/opt/nvm/nvm.sh
 
 if status --is-interactive
     iTerm2_integration
